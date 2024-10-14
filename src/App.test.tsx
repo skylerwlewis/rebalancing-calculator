37661a6e9c2cd75e6c30d7167f7630315434e16a
Skyler Lewis <skyler.w.lewis@gmail.com>
2024-10-14 15:41:59 -0500

    Switching from CRE to vite react-ts

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders rebalancing message', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Fill in your fund information to rebalance your portfolio/i);
-  expect(linkElement).toBeInTheDocument();
-});
